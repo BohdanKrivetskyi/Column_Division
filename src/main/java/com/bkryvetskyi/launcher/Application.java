da8cb8cc0021e949acc7362727a2f303c864ff17
Bohdan Kryvetskyi <krivettskiy@gmail.com>
2023-04-06 16:01:47 +0300

    Initial commit.

---
+++ src/main/java/com/bkryvetskyi/launcher/Application.java
@@ -0,0 +1,16 @@
+package com.bkryvetskyi.launcher;
+
+import com.bkryvetskyi.service.ConsoleInputReader;
+import com.bkryvetskyi.service.DivisionPrinter;
+
+
+public class Application {
+    public static void main(String[] args) {
+        run();
+    }
+    private static void run() {
+        ConsoleInputReader input = new ConsoleInputReader();
+        DivisionPrinter divisionPrinter = new DivisionPrinter();
+        divisionPrinter.columnDivision(input.readInt(), input.readInt());
+    }
+}
