8aba8a0428c6a9e6b4b6efcf91a9ab3dd7493811
Bohdan Kryvetskyi <krivettskiy@gmail.com>
2023-04-14 23:51:43 +0300

    Added new method for calculate divisor.

--- src/main/java/com/bkryvetskyi/service/DivisionPrinter.java
+++ src/main/java/com/bkryvetskyi/service/DivisionPrinter.java
@@ -6,95 +6,74 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.List;
 
 
 public class DivisionPrinter {
-    private static final Logger logger = LogManager.getLogger(DivisionPrinter.class.getName());
-    private static final String UNDERLINE = "_";
-    private static final String STRAIGHT_SLASH = "|";
-    private static final String DASH = "-";
 
-    // Method to print the division calculation columns.
-    public void printColumnDivision(int dividend, int divisior) {
-
-        // Convert the dividend to an ArrayList of its digits.
-        ArrayList<Integer> dividendDigitsArray = new ArrayList<>();
-        addNumberDigitsToList(dividendDigitsArray, dividend);
-
-        // Calculate quotient and remainder.
-        int quotient = dividend / divisior;
-        int remainder = dividend % divisior;
-
-        // Initialize digitsDividendIndex with the first digit of the dividend
-        int digitsDividendIndex = dividendDigitsArray.get(0);
-
-        // Calculate the optimal divisor for subtraction and store the result in digitsDividend
-        int digitsDividend =
-                calculateOptimalDivisorForSubtraction(dividendDigitsArray, divisior, digitsDividendIndex);
-        // Find the digits for multiplication and store the result in multiplication.
-        int multiplication = findDigitsForMultiplication(digitsDividend, divisior);
-        // Calculate the difference between digitsDividend
-        // and multiplication and store the result in difference.
-        int difference = digitsDividend - multiplication;
-
-
-        // Print the division calculation columns.
-        logger.info("{}{}{}{}", UNDERLINE, StringUtils.leftPad(String.valueOf(dividend),
-                dividendDigitsArray.size()), STRAIGHT_SLASH, divisior);
-
-        logger.info("{}{}{}{}",
-                " ", multiplication, StringUtils.leftPad(STRAIGHT_SLASH,
-                        dividendDigitsArray.size() - String.valueOf(multiplication).length() + 1),
-                StringUtils.repeat(DASH, String.valueOf(quotient).length()));
-
-        logger.info("{}{}{}{}",
-                " ", StringUtils.repeat(DASH, String.valueOf(multiplication).length()),
-                StringUtils.leftPad(STRAIGHT_SLASH,
-                        dividendDigitsArray.size() - String.valueOf(multiplication).length() + 1), quotient);
-
-        logger.info("{}{}{}", UNDERLINE, difference);
-    }
-
-    // Method to convert a number's digits into an ArrayList.
-    private ArrayList<Integer> addNumberDigitsToList(ArrayList<Integer> arrayDigitsInt, int digits) {
-
-        // Extract the digits from the number and add them to the ArrayList
-        while (digits > 0) {
-            arrayDigitsInt.add(digits % 10);
-            digits /= 10;
-        }
-        // Reverse the order of the digits in the ArrayList.
-        Collections.reverse(arrayDigitsInt);
-
-        return arrayDigitsInt;
-    }
 
     // Method to calculate the optimal divisor for subtraction.
-    private int calculateOptimalDivisorForSubtraction(ArrayList<Integer> dividendDigitsArray,
-                                                      int divisior,
-                                                      int digitDividend) {
-        // Set digitDividend to the first digit of the dividend.
-        Iterator<Integer> iterator = dividendDigitsArray.listIterator(digitDividend);
-        // Use an iterator to calculate the optimal divisor for subtraction.
-        while (iterator.hasNext() && digitDividend < divisior) {
-            digitDividend = digitDividend * 10 + iterator.next();
-        }
+    public List<Integer> calculateOptimalDivisorForSubtraction(String dividend, int divisior) {
 
-        return digitDividend;
+        // Start with the first digit of the dividend.
+        int digitResult = Character.getNumericValue(dividend.charAt(0));
+        // Variable to store the result of multiplication.
+        int multiplication;
+
+        // Create a list to store the results.
+        List<Integer> result = new ArrayList<>();
+
+        // Loop through the dividend, starting from the second digit.
+        for (int i = 1; i < dividend.length(); ) {
+
+            // If the current digit result is less than the divisor,
+            // add the next digit and continue to the next iteration.
+            if (digitResult < divisior) {
+                digitResult = digitResult * 10 + Character.getNumericValue(dividend.charAt(i));
+                i++;
+
+                // If the digit result is now greater than the divisor,
+                // add it to the results list.
+                if (digitResult > divisior) {
+                    result.add(digitResult);
+                }
+            } else {
+                // If the current digit result is greater than or equal to the divisor,
+                // find the digits for multiplication, add it to the results list,
+                // and subtract the result from the current digit.
+                multiplication = findDigitsForMultiplication(digitResult, divisior);
+                result.add(multiplication);
+                digitResult -= multiplication;
+            }
+        }
+        // If the final digit result is less than the divisor,
+        // return the results list.
+        if (digitResult < divisior) {
+            return result;
+        } else {
+            // If the final digit result is greater than or equal to the divisor,
+            // find the digits for multiplication, add it to the results list,
+            // subtract the result from the current digit, and add the final digit
+            // result to the results list.
+            multiplication = findDigitsForMultiplication(digitResult, divisior);
+            result.add(multiplication);
+            digitResult = digitResult - multiplication;
+            result.add(digitResult);
+        }
+        return result;
     }
 
     // Method to find the digits for multiplication.
-    private int findDigitsForMultiplication(int digitDivident, int divisior) {
+    public int findDigitsForMultiplication(int digitDividend, int divisior) {
 
         int multiplication = 0;
         // Find the largest multiple of the divisor that is less than or equal to the digitsDividend
         for (int k = 9; k > 0; k--) {
             multiplication = divisior * k;
-            if (digitDivident >= multiplication) break;
+            if (digitDividend >= multiplication) break;
         }
 
+        // Return the final results list.
         return multiplication;
     }
 }
