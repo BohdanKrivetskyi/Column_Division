4d87d815d69012b3054d3cccd211790053344908
Bohdan Kryvetskyi <krivettskiy@gmail.com>
2023-04-17 20:43:45 +0300

    Added more tests.

--- src/test/java/com/bkryvetskyi/service/ConsoleInputReaderTest.java
+++ src/test/java/com/bkryvetskyi/service/ConsoleInputReaderTest.java
@@ -1,5 +1,6 @@
 package com.bkryvetskyi.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -8,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ConsoleInputReaderTest {
     @Test
-    void testReadIntValidInput() {
+    void ReadIntValidInput() {
         // Arrange
         String input = "123\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
@@ -22,7 +23,7 @@ class ConsoleInputReaderTest {
     }
 
     @Test
-    void testReadIntInvalidInputNegativeNumber() {
+    void ReadIntInputNegativeNumber() {
         // Arrange
         String input = "-5\n10\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
@@ -36,7 +37,7 @@ class ConsoleInputReaderTest {
     }
 
     @Test
-    void testReadIntInvalidInputNonInteger() {
+    void ReadIntInvalidInputNonInteger() {
         // Arrange
         String input = "abc\n15\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
@@ -50,7 +51,7 @@ class ConsoleInputReaderTest {
     }
 
     @Test
-    void testReadIntInvalidInputEmptyInput() {
+    void ReadIntInvalidInputEmptyInput() {
         // Arrange
         String input = "\n25\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
@@ -63,4 +64,57 @@ class ConsoleInputReaderTest {
         assertEquals(25, result);
     }
 
+    @Test
+    void ReadIntInvalidInputNoInteger() {
+        // Arrange
+        String input = "abc\n15\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        ConsoleInputReader reader = new ConsoleInputReader();
+
+        // Act
+        int result = reader.readInt();
+
+        // Assert
+        assertEquals(15, result);
+    }
+
+    @Test
+    void ReadIntInvalidInputGreaterThanInt() {
+        // Arrange
+        String input = "2147483648\n10\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        ConsoleInputReader reader = new ConsoleInputReader();
+
+        // Act
+        int result = reader.readInt();
+
+        // Assert
+        assertEquals(10, result);
+    }
+
+    @Test
+    void ReadIntInvalidInputNegativeGreaterThanInt() {
+        // Arrange
+        String input = "-2147483649\n10\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        ConsoleInputReader reader = new ConsoleInputReader();
+
+        // Act
+        int result = reader.readInt();
+
+        // Assert
+        assertEquals(10, result);
+    }
+
+    @Test
+    void closeClosesScanner() {
+        // Arrange
+        ConsoleInputReader reader = new ConsoleInputReader();
+
+        // Act
+        reader.close();
+
+        // Assert
+        assertThrows(IllegalStateException.class, () -> reader.readInt());
+    }
 }
